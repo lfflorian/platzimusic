42d0b5c94dbf15225b30ae4756f1a50b8c9c2297
Luis Florian <lfflorian4@gmail.com>
2017-09-11 09:29:46 -0600

    inicial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: 'f91712f3bf93d471462a8891f2df2ff4',
+	secret: 'b6efdc51ed2cfdb41ca5951450fa79bf',
+	registeredTo: 'lfflorian'
+}
+
+
+		// Application name	platzimusic
+		// API key	f91712f3bf93d471462a8891f2df2ff4
+		// Shared secret	b6efdc51ed2cfdb41ca5951450fa79bf
+		// Registered to	lfflorian
---
+++ src/componens/Artist.vue
@@ -0,0 +1,21 @@
+li(v-for="artist in artists") {{artist.name}}
+
+<template lang="pug">
+	li
+		h2: a(v-bind:href="artist.url" target="_blank") {{ artist.name }}
+		img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+import getArtists from '../api'
+export default {
+	name: 'artist',
+	props: ['artist']
+}
+</script>
+
+<style lang="stylus" scoped>
+li 
+	display block
+	margin 10px 0
+</style>
